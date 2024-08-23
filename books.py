217b37727e38a3cc9dafd3b4657170d161c0e47e
davidconnor-ont <85928647+davidconnor-ont@users.noreply.github.com>
2024-08-23 09:52:12 +0100

    Add files via upload

---
+++ books.py
@@ -0,0 +1,17 @@
+# Define a class that can be initialised with an array of tuples ( title, author)
+# Implement by_authors method that will print authors and the books they have written
+# Example input : 
+#  
+#[
+#    ('Foundation', 'Isimov'),
+#    ('Foundation and Empire', 'Isimov' ),
+#    ('War and Peace', 'Tolstoy'),
+#    ('Anna Karenina', 'Tolstoy')
+#]
+
+# Example output
+#Example output
+#{
+#  'Isimov': [ 'Foundation', 'Foundation and Empire']
+#  'Tolstoy': [ 'War and Peace',  'Anna Karenina']
+#}
---
+++ strings.py
@@ -0,0 +1,11 @@
+# Create a function that accepts a string of unknown length.
+# 
+# The function should print a list of unique string triplets and the number of times that triplet occurs.
+# 
+# For example, if you were given 'AAAABCBCB', the output would be:
+# 
+# AAA = 2
+# AAB = 1
+# ABC = 1
+# BCB = 2
+# CBC = 1
